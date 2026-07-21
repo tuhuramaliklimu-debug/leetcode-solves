fcc2f37a79a918b84f6b5d6907acca9a0e501829
tuhuramaliklimu-debug <tuhuramaliklimu@gmail.com>
2026-07-21 13:11:21 +0600

    Time: 1 ms (75.23%), Space: 65 MB (69.95%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,24 @@
+//used index marking tecnique...frequency counting using modulo..the idea is to use  the input array itself to store how many times each each numbers appears without using extra space
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int max_idx=0;
+        int curr_max=0;
+        for(int i=0;i<n;i++)
+        {
+            int id=nums[i]%n;
+            nums[id]+=n;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>curr_max)
+            {
+                curr_max=nums[i];
+                max_idx=i;
+            }
+            nums[i]%=n;
+        }
+        return max_idx;
+    }
+};
