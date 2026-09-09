fc365d11511f1e80a3165e86d87e2a007cde7928
tuhuramaliklimu-debug <tuhuramaliklimu@gmail.com>
2026-09-09 22:43:36 +0600

    Time: 0 ms (100%), Space: 9.1 MB (57.33%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long p=1000;
+        long long res=0;
+        while(p<=n){
+            res+=n-p+1;
+            p*=1000;
+        }
+        return res;
+        
+    }
+};
